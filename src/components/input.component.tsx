58913e39624bc95baba714fcab83cf0e310c8b34
Joseph Dvorak <dvorakjt@gmail.com>
2023-08-31 21:41:32 -0400

    added additional props to Input component

--- src/components/input.component.tsx
+++ src/components/input.component.tsx
@@ -7,17 +7,47 @@ export type InputProps = {
   inputType : string;
   inputClassName : string;
   readOnly? : boolean;
+  autoComplete? : string;
+  placeholder? : string;
+  list? : string;
+  autoFocus? : boolean;
+  step? : number;
+  max? : string;
+  min? : string;
+  maxLength? : number;
+  size? : number;
 }
 
-export function Input({fieldName, inputType, inputClassName, readOnly = false} : InputProps) {
+//need to include aria-invalid and aria-errormessage, these should only be set after the form has been submitted
+//these props should be added to InputGroup
+export function Input({fieldName, inputType, inputClassName, readOnly = false, autoComplete, placeholder, list, autoFocus, step, max, min, maxLength, size} : InputProps) {
   const formCtx = useContext(FormContext);
   if(formCtx === null) throw new Error('Input cannot access property useField of null FormContext');
   else {
     const { useField } = formCtx;
     const { value, validity, updateValue } = useField(fieldName);
     
-    return <input id={fieldName} type={inputType} className={inputClassName} readOnly={readOnly} data-validity={validityToString(validity)} value={value} onChange={(e) => {
-      updateValue(e.target.value);
-    }} />
+    return (
+    <input 
+      id={fieldName}
+      name={fieldName}
+      type={inputType}
+      className={inputClassName} 
+      data-validity={validityToString(validity)} 
+      value={value} 
+      onChange={(e) => {
+       updateValue(e.target.value);
+      }}
+      readOnly={readOnly} 
+      autoComplete={autoComplete}
+      placeholder={placeholder}
+      list={list}
+      autoFocus={autoFocus}
+      step={step}
+      max={max}
+      min={min}
+      maxLength={maxLength}
+      size={size}
+    />);
   }
 }
