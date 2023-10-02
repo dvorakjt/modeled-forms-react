5a59bc40a79d73d675559c5b49b4515bfc3d246c
Joseph Dvorak <dvorakjt@gmail.com>
2023-10-02 19:51:54 -0400

    completed includes-symbol test suite

--- src/__test__/model/validators/util/includes-symbol.test.ts
+++ src/__test__/model/validators/util/includes-symbol.test.ts
@@ -1,5 +1,6 @@
 import { describe, test, expect } from 'vitest';
 import { includesSymbol } from '../../../../model';
+import { container } from '../../../../model/container';
 
 const symbols = "!\"#$%&'()*+,-./\\:;<=>?@[]^_`{|}~".split('');
 
@@ -15,6 +16,51 @@ describe('includesSymbol', () => {
   test('It accepts internal whitespace as a symbol when autoTrim is true in the config file.', () => {
     expect(includesSymbol('')('a b')).toStrictEqual({
       isValid : true
+    });
+  });
+
+  test('It returns an object whose isValid property is set to false when it receives a string that does not contain a symbol.', () => {
+    const str = 'ABCDefg0123456789';
+    expect(includesSymbol('test message')(str)).toStrictEqual({
+      isValid : false,
+      message : 'test message'
+    });
+  });
+
+  test('It trims the provided string before evaluating it if autoTrim is set to true in the config file.', () => {
+    expect(includesSymbol('test message')(' ')).toStrictEqual({
+      isValid : false,
+      message : 'test message'
+    });
+  });
+
+  test('It does not trim the provided string before evaluating it if autoTrim is set to false in the config file.', () => {
+    const configLoader = container.services.ConfigLoader;
+    configLoader.config.autoTrim = false;
+
+    expect(includesSymbol('')(' ')).toStrictEqual({
+      isValid : true
+    });
+
+    configLoader.config.autoTrim = true;
+  });
+
+  test('It utilizes the regex provided in the config file.', () => {
+    const configLoader = container.services.ConfigLoader;
+    const tempSymbolRegex = configLoader.config.symbolRegex;
+    configLoader.config.symbolRegex = /.*/;
+
+    expect(includesSymbol('')('a')).toStrictEqual({
+      isValid : true
+    });
+
+    configLoader.config.symbolRegex = tempSymbolRegex;
+  });
+
+  test('When a success message is passed in as an argument, that message is returned should the provided string pass.', () => {
+    expect(includesSymbol('', 'test success message')('!')).toStrictEqual({
+      isValid : true,
+      message : 'test success message'
     });
   });
 });
